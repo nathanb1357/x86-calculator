8c260e81ca64cc638ae5073420ca49ea7b53180f
nathanb1357 <nathanb1357@gmail.com>
2023-04-05 00:24:25 -0700

    Created simple calculator

--- calculator.c
+++ calculator.c
@@ -1,38 +1,62 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
+#include <ctype.h>
 
-const char* operators = "+-*/()%%^";
-
-
-//
-double add(double num, char* next) {
-
+int get_num(char** str) {
+    int num = 0;
+    while (isdigit(**str)) {
+        num = num * 10 + (**str - '0');
+        (*str)++;
+    }
+    return num;
 }
 
-int getType(char c)
-{
-    if (c >= '0' && c <= '9') {return 1;}
-    else if (strchr(operators, c) != NULL) {return 2;}
-    printf("ERROR: Invalid Operation\n");
-    exit(1);
+int calculate_term(char** str) {
+    int num1 = get_num(str);
+    while (**str) {
+        if (**str == '*') {
+            (*str)++;
+            int num2 = get_num(str);
+            num1 *= num2;
+        } else if (**str == '/') {
+            (*str)++;
+            int num2 = get_num(str);
+            num1 /= num2;
+        } else {
+            break;
+        }
+    }
+    return num1;
 }
 
-double parse(char* string, int start, int end)
-{
-
+int calculate(char** str) {
+    int num1 = calculate_term(str);
+    while (**str) {
+        if (**str == '+') {
+            (*str)++;
+            int num2 = calculate_term(str);
+            num1 += num2;
+        } else if (**str == '-') {
+            (*str)++;
+            int num2 = calculate_term(str);
+            num1 -= num2;
+        } else {
+            break;
+        }
+    }
+    return num1;
 }
 
-int main(int argc, char** argv)
+int main(int argc, char *argv[])
 {
     if (argc != 2) {
-        printf("ERROR: Invalid Number of Arguments\n");
+        printf("ERROR: Invalid number of arguments\n");
         exit(1);
     }
-    
-    const char* string = argv[1];
-    double result = parse(string, 0, sizeof(string));
-    printf("\n\nYour answer is:\n%d", result);
 
+    char* expression = argv[1];
+    int result = calculate(&expression);
+    printf("RESULT: %d\n", result);
     return 0;
 }
