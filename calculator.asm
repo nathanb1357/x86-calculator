8ee31684f49024661a3e9dbe0f27da5268198ff3
nathanb1357 <nathanb1357@gmail.com>
2023-04-08 15:16:22 -0700

    Added error message

--- calculator.asm
+++ calculator.asm
@@ -1,5 +1,21 @@
 section .data
-    format db "RESULT: ", 7
+    argError db "ERROR: Invalid number of arguments", 0ah
 
-section .bss
-    input resb 8
+section .text
+    global _start
+    
+; 1. Get input and check if there are any spaces
+; 2. Assign input to 'expression' (64 bytes?? or 128??)
+; 3. Call calculate and store that value in 'result'
+; 4. Print out result
+; 5. Exit with code 0
+
+_start:
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, argError
+    mov rdx, 35
+    syscall
+    mov rax, 60
+    mov rdi, 1
+    syscall
